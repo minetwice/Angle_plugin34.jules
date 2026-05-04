f5b4ae9bfc9015b0450345a3a875ed0bb401dc2d
Twicefear <ytd82774@gmail.com>
2026-05-04 12:48:47 +0530

    Create AbsorptionGuiListener.java

---
+++ src/main/java/com/diablosmp/plugin/listeners/AbsorptionGuiListener.java
@@ -0,0 +1,7 @@
+package com.diablosmp.plugin.listeners;
+
+import org.bukkit.event.Listener;
+
+public class AbsorptionGuiListener implements Listener {
+    // Listens for clicks in the absorption GUI
+}
