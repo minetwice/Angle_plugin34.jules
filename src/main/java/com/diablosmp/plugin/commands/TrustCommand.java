aca244034aa6e4a1ace4ab3623129482d03cff33
Twicefear <ytd82774@gmail.com>
2026-05-04 13:01:39 +0530

    Update TrustCommand.java

--- src/main/java/com/diablosmp/plugin/commands/TrustCommand.java
+++ src/main/java/com/diablosmp/plugin/commands/TrustCommand.java
@@ -1,12 +1,24 @@
 package com.diablosmp.plugin.commands;
 
+import com.diablosmp.plugin.managers.TrustManager;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class TrustCommand implements CommandExecutor {
+    private final TrustManager trustManager;
+    public TrustCommand(TrustManager trustManager) { this.trustManager = trustManager; }
+
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return true;
+    public boolean onCommand(org.bukkit.command.CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player p && args.length == 1) {
+            Player target = Bukkit.getPlayer(args[0]);
+            if (target != null && target != p) {
+                trustManager.createContract(p, target);
+                return true;
+            }
+        }
+        return false;
     }
 }
