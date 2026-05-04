3716fa9c40a5a636503946aa28c3baf22b6882cb
Twicefear <ytd82774@gmail.com>
2026-05-04 12:49:22 +0530

    Update TrustManager.java

--- src/main/java/com/diablosmp/plugin/managers/TrustManager.java
+++ src/main/java/com/diablosmp/plugin/managers/TrustManager.java
@@ -1,7 +1,5 @@
 package com.diablosmp.plugin.managers;
 
 public class TrustManager {
-    public TrustManager() {
-        // TODO: Initialize trust contract logic
-    }
+    // Manages player trust contracts
 }
