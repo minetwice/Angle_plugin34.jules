564a0669e3e79eda410030ce8c97d38a79570d3e
Twicefear <ytd82774@gmail.com>
2026-05-04 12:48:38 +0530

    Create ItemSecurityListener.java

---
+++ src/main/java/com/diablosmp/plugin/listeners/ItemSecurityListener.java
@@ -0,0 +1,7 @@
+package com.diablosmp.plugin.listeners;
+
+import org.bukkit.event.Listener;
+
+public class ItemSecurityListener implements Listener {
+    // Drop, store, death events for Diablo items
+}
