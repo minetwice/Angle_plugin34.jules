72b4dc19051cc2903f610d8d1ed1790f8a5770b6
Twicefear <ytd82774@gmail.com>
2026-05-04 12:49:23 +0530

    Update DiabloCommand.java

--- src/main/java/com/diablosmp/plugin/commands/DiabloCommand.java
+++ src/main/java/com/diablosmp/plugin/commands/DiabloCommand.java
@@ -3,11 +3,17 @@ package com.diablosmp.plugin.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import java.util.List;
 
-public class DiabloCommand implements CommandExecutor {
+public class DiabloCommand implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage("Diablo admin command executed.");
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return null;
     }
 }
