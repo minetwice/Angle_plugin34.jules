4a6e6f7117160d27f8b2a3e260661e185beea2b6
Twicefear <ytd82774@gmail.com>
2026-05-04 12:49:20 +0530

    Update DiabloPlugin.java

--- src/main/java/com/diablosmp/plugin/DiabloPlugin.java
+++ src/main/java/com/diablosmp/plugin/DiabloPlugin.java
@@ -1,35 +1,15 @@
 package com.diablosmp.plugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import com.diablosmp.plugin.managers.AbilityManager;
-import com.diablosmp.plugin.managers.TrustManager;
-import com.diablosmp.plugin.commands.DiabloCommand;
-import com.diablosmp.plugin.commands.TrustCommand;
 
 public class DiabloPlugin extends JavaPlugin {
-
-    private static DiabloPlugin instance;
-    private AbilityManager abilityManager;
-    private TrustManager trustManager;
-
     @Override
     public void onEnable() {
-        instance = this;
-        this.abilityManager = new AbilityManager();
-        this.trustManager = new TrustManager();
-
-        getCommand("diablo").setExecutor(new DiabloCommand());
-        getCommand("trust").setExecutor(new TrustCommand());
-
-        getLogger().info("DiabloPlugin has been enabled!");
+        // Plugin startup logic
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("DiabloPlugin has been disabled!");
-    }
-
-    public static DiabloPlugin getInstance() {
-        return instance;
+        // Plugin shutdown logic
     }
 }
