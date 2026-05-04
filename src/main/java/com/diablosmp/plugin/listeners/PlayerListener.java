72bf1cc83705e5ec485147c3ca98ab74ae9c6302
Twicefear <ytd82774@gmail.com>
2026-05-04 12:48:36 +0530

    Create PlayerListener.java

---
+++ src/main/java/com/diablosmp/plugin/listeners/PlayerListener.java
@@ -0,0 +1,7 @@
+package com.diablosmp.plugin.listeners;
+
+import org.bukkit.event.Listener;
+
+public class PlayerListener implements Listener {
+    // Player interactions (sneak, left-click entity)
+}
